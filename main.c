3eba4c8a271df0cee8c50b101bf99f5c00160656
Harsha192 <harshagayan8@gmail.com>
2018-10-16 00:18:51 +0530

    new branch devide added

--- main.c
+++ main.c
@@ -7,12 +7,19 @@
  void sustract(int a, int b){
 	 return (a-b);
  }
- 
+ void multiply(int a, int b){
+	 return (a*b);
+ }
+ void devide(int a, int b){
+	 return (a/b);
+ }
 int main() {
 
   printf("Hello World\n");
   printf(add(1,2));
   printf(sustract(1,2));
+  printf(multiply(1,2));
+  printf(devide(1,2));
   return 0;
 
 }
