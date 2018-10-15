7d03d2bdad342cc2544058945273337f530b52b5
Harsha192 <harshagayan8@gmail.com>
2018-10-15 23:55:24 +0530

    new branch second commit

--- main.c
+++ main.c
@@ -4,10 +4,15 @@
  void add(int a, int b){
 	return (a+b);
  }
+ void sustract(int a, int b){
+	 return (a-b);
+ }
+ 
 int main() {
 
   printf("Hello World\n");
   printf(add(1,2));
+  printf(sustract(1,2));
   return 0;
 
 }
